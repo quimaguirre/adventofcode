ee732b05ed7af1cb6ce768c6213c2d8774d0cc4e
quimaguirre <quim.aguirre@hotmail.com>
2024-12-02 16:59:56 +0100

    feat(day01.py): include calculation of similarity for part 2

--- 2024/day01/day01.py
+++ 2024/day01/day01.py
@@ -3,6 +3,8 @@ import pandas as pd
 # Read input data frame
 input_df = pd.read_csv("input.txt", sep = '\\s+', header = None, names = ['loc1', 'loc2'])
 
+### PART 1 ###
+
 # Sort all columns using apply sort_values
 input_df = input_df.apply(lambda x: x.sort_values().values)
 
@@ -12,3 +14,18 @@ print(input_df)
 
 # Calculate total distance
 print(sum(input_df['diff']))
+
+
+### PART 2 ###
+
+# Count values in loc 2
+loc2_counts = input_df['loc2'].value_counts().rename_axis('loc2').reset_index(name='counts')
+
+# Merge counts with loc1
+similarity_df = pd.merge(input_df[["loc1"]], loc2_counts, how = "inner", left_on = 'loc1', right_on = 'loc2')[['loc1', 'counts']]
+
+# Calculate similarity score
+similarity_df['similarity'] = similarity_df['loc1'] * similarity_df['counts']
+
+# Calculate total similarity
+print(sum(similarity_df['similarity']))
