562293616e945b73c18a459cbffaf39ca3695f91
quimaguirre <quim.aguirre@hotmail.com>
2024-12-02 16:35:14 +0100

    feat(day01.py): sorting, difference and sum

--- 2024/day01/day01.py
+++ 2024/day01/day01.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
-input_df = pd.read_csv("input.txt", sep = "\t", header = None, names = ["loc1", "loc2"])
-input_df.sort_values(by=['loc1', 'loc2'], ascending = True)
+# Read input data frame
+input_df = pd.read_csv("input.txt", sep = '\\s+', header = None, names = ['loc1', 'loc2'])
+
+# Sort all columns using apply sort_values
+input_df = input_df.apply(lambda x: x.sort_values().values)
+
+# Calculate difference
+input_df['diff'] = abs(input_df['loc1'] - input_df['loc2'])
 print(input_df)
+
+# Calculate total distance
+print(sum(input_df['diff']))
