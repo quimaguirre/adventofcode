6fca2debd851e61ee0ff34ee64149238ffc4708a
quimaguirre <quim.aguirre@hotmail.com>
2024-12-05 17:33:50 +0100

    feat(day05/): approach working for small data of part 2

--- 2024/day05/day05.py
+++ 2024/day05/day05.py
@@ -3,38 +3,41 @@ import math
 update_to_children = {}
 correct_update_middle_nums = []
 corrected_update_middle_nums = []
-with open('small_input.txt') as input_fd:
+with open('input.txt') as input_fd:
     for line in input_fd:
         if '|' in line:
+            # Save the children of each number
             updates = line.strip().split('|')
             update_to_children.setdefault(updates[0], []).append(updates[1])
         elif ',' in line:
             updates = line.strip().split(',')
             prev_updates = []
-            updates_corrected = [x for x in updates]
-            correct_updates = True
+            updates_to_be_corrected = [x for x in updates]
+            correct = True
             for update in updates:
                 if update in update_to_children:
+                    # Check if the previous updates are children of the current update
                     children_updates = update_to_children[update]
                     if all([prev_update not in children_updates for prev_update in prev_updates]):
-                        prev_updates.append(update)
+                        pass
                     else:
-                        correct_updates = False
-                        print(f"Incorrect updates: {updates}! Starting correction...")
-                        incorrect_prev_updates = [prev_update in children_updates for prev_update in prev_updates]
-                        updates_before_correction = updates.split(incorrect_prev_updates[0])[0]
-                        updates_corrected = updates_before_correction + [update] + incorrect_prev_updates
-                        continue
-                else:
-                    prev_updates.append(update)
-            if correct_updates:
+                        correct = False
+                        incorrect_prev_updates = [prev_update for prev_update in prev_updates if prev_update in children_updates]
+                        print(f"Incorrect updates: {updates_to_be_corrected}! Problem at {update} with previous updates {incorrect_prev_updates}. Starting correction...")
+                        updates_before_correction = [x for x in updates_to_be_corrected[:updates_to_be_corrected.index(update)] if x not in incorrect_prev_updates]
+                        print(updates_before_correction)
+                        updates_after_correction = updates_to_be_corrected[(updates_to_be_corrected.index(update) + 1):]
+                        print(updates_after_correction)
+                        updates_to_be_corrected = updates_before_correction + [update] + incorrect_prev_updates + updates_after_correction
+                prev_updates.append(update)
+            if correct:
                 middle_num = int(updates[math.ceil(len(updates)/2) - 1])
                 correct_update_middle_nums.append(middle_num)
                 print(f"Correct updates: {updates}. Middle num: {middle_num}")
             else:
-                middle_num = int(updates_corrected[math.ceil(len(updates_corrected)/2) - 1])
+                middle_num = int(updates_to_be_corrected[math.ceil(len(updates_to_be_corrected)/2) - 1])
                 corrected_update_middle_nums.append(middle_num)
-                print(f"Corrected updates: {updates_corrected}. Middle num: {middle_num}")
+                print(f"Corrected updates: {updates_to_be_corrected}. Middle num: {middle_num}")
 
 #print(update_to_children)
 print(sum(correct_update_middle_nums))
