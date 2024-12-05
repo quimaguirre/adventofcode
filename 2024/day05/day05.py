46dfca5cc7fadc12642ee894b71b7f0fde1ef694
quimaguirre <quim.aguirre@hotmail.com>
2024-12-05 17:47:10 +0100

    fix(day05.py): retrieve incorrect previous updates in the order of the updates

--- 2024/day05/day05.py
+++ 2024/day05/day05.py
@@ -22,14 +22,15 @@ with open('input.txt') as input_fd:
                         pass
                     else:
                         correct = False
-                        incorrect_prev_updates = [prev_update for prev_update in prev_updates if prev_update in children_updates]
+                        # Get incorrect previous updates in the same order as the updates
+                        incorrect_prev_updates = [x for x in updates_to_be_corrected if x in prev_updates and x in children_updates]
                         print(f"Incorrect updates: {updates_to_be_corrected}! Problem at {update} with previous updates {incorrect_prev_updates}. Starting correction...")
+                        # Paste updates in the new order
                         updates_before_correction = [x for x in updates_to_be_corrected[:updates_to_be_corrected.index(update)] if x not in incorrect_prev_updates]
-                        print(updates_before_correction)
                         updates_after_correction = updates_to_be_corrected[(updates_to_be_corrected.index(update) + 1):]
-                        print(updates_after_correction)
                         updates_to_be_corrected = updates_before_correction + [update] + incorrect_prev_updates + updates_after_correction
                 prev_updates.append(update)
+            # Get middle number of the updates
             if correct:
                 middle_num = int(updates[math.ceil(len(updates)/2) - 1])
                 correct_update_middle_nums.append(middle_num)
@@ -39,6 +40,5 @@ with open('input.txt') as input_fd:
                 corrected_update_middle_nums.append(middle_num)
                 print(f"Corrected updates: {updates_to_be_corrected}. Middle num: {middle_num}")
 
-#print(update_to_children)
 print(sum(correct_update_middle_nums))
 print(sum(corrected_update_middle_nums))
