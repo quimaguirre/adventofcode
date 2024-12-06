35c3cd91dac469210cec4b42afcf2f3128319dc8
Quim Aguirre <quim.aguirre@hotmail.com>
2024-12-06 12:07:54 +0100

    feat(day06/): adapt code to solution for part 2

--- 2024/day06/day06.py
+++ 2024/day06/day06.py
@@ -1,3 +1,5 @@
+import copy
+
 def get_next_direction(direction):
     if direction == "up":
         next_direction = "right"
@@ -20,37 +22,61 @@ def guard_move(curr_coords, direction):
         next_coords = (curr_coords[0], curr_coords[1] + 1)
     return next_coords
 
+def get_patrol_positions(patrol_map, start_coords, start_direction = "up", no_new_positions_limit = 1000):
+    direction = start_direction
+    patrol_active = True
+    visited_positions = {start_coords}
+    curr_coords = (start_coords[0], start_coords[1])
+    no_new_positions_count = 0
+    while patrol_active:
+        searching_next_location = True
+        while searching_next_location:
+            next_coords = guard_move(curr_coords, direction)
+            if (next_coords[0] >= 0 and next_coords[0] < len(patrol_map)) and (next_coords[1] >= 0 and next_coords[1] < len(patrol_map[0])):
+                next_location = patrol_map[next_coords[0]][next_coords[1]]
+                if next_location == '#':
+                    direction = get_next_direction(direction)
+                else:
+                    searching_next_location = False
+                    curr_coords = (next_coords[0], next_coords[1])
+                    if curr_coords in visited_positions:
+                        no_new_positions_count += 1
+                    else:
+                        no_new_positions_count = 0
+                        visited_positions.add(curr_coords)
+            else:
+                searching_next_location = False
+                patrol_active = False
+        if no_new_positions_count >= no_new_positions_limit:
+            return {}
+    return visited_positions
+
+# Read input data and save starting position
 patrol_map = []
-curr_coords = None
+start_coords = None
 with open('input.txt') as input_fd:
     for i, line in enumerate(input_fd):
         positions = [pos for pos in line.strip()]
         patrol_map.append(positions)
         if '^' in positions:
-            curr_coords = (i, positions.index('^'))
-curr_location = patrol_map[curr_coords[0]][curr_coords[1]]
-#print(curr_coords)
-#print(curr_location)
+            start_coords = (i, positions.index('^'))
+start_location = patrol_map[start_coords[0]][start_coords[1]]
+#print(start_coords)
+#print(start_location)
 
-direction = "up"
-patrol_active = True
-visited_positions = {curr_coords}
-while patrol_active:
-    searching_next_location = True
-    while searching_next_location:
-        next_coords = guard_move(curr_coords, direction)
-        if (next_coords[0] >= 0 and next_coords[0] < len(patrol_map)) and (next_coords[1] >= 0 and next_coords[1] < len(patrol_map[0])):
-            next_location = patrol_map[next_coords[0]][next_coords[1]]
-            if next_location == '#':
-                direction = get_next_direction(direction)
-            else:
-                searching_next_location = False
-                curr_coords = (next_coords[0], next_coords[1])
-                visited_positions.add(curr_coords)
-                #print(curr_coords)
-                #print(next_location)
-        else:
-            searching_next_location = False
-            patrol_active = False
-
+# Part 1: Determine all unique positions visited by the guard until she falls
+# off the grid
+visited_positions = get_patrol_positions(patrol_map, start_coords)
 print(len(visited_positions))
+
+# Part 2: Check all possible positions in which an object creates a loop
+loop_positions = set()
+for i, positions in enumerate(patrol_map):
+    for j, location in enumerate(patrol_map[i]):
+        if (i, j) != start_coords and location != "#":
+            new_patrol_map = copy.deepcopy(patrol_map)
+            new_patrol_map[i][j] = "#"
+            visited_positions = get_patrol_positions(new_patrol_map, start_coords)
+            if len(visited_positions) == 0:
+                loop_positions.add((i, j))
+print(len(loop_positions))
